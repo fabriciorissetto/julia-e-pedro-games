19a76b452ef1dc87a068609638fa076036a0b7e0
Fabrício Rissetto <fabriciorissetto@gmail.com>
2026-05-21 20:05:54 -0300

    gemeos: cria Jump Rex (dino runner com cubos de poder e loja de skins)

--- gemeos/games.js
+++ gemeos/games.js
@@ -1,1 +1,9 @@
-window.GAMES = [];
+window.GAMES = [
+  {
+    slug: "jump-rex",
+    title: "Jump Rex",
+    emoji: "🦖",
+    color: "#9b6fff",
+    description: "Corre, pula, coleta cubos. Cubo roxo = super pulo."
+  },
+];
